53ec517bdaf85eec35ba54737703f5ae2b796e35
giogaf <giogaf@gmail.com>
2016-06-09 15:05:21 -0500

    custom size image,style gem twitter-bootstrap-rails

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,11 @@
 class Image < ActiveRecord::Base
 	mount_uploader :archivo, ArchivoUploader
+	validate :archivo_menor_100kb
+
+	private
+	def archivo_menor_100kb
+		if archivo.size > 100.kilobytes
+			errors.add :archivo,"tamaño archivo debe ser menor a 100KB"
+		end
+	end
 end
